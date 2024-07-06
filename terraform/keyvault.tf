9d1cd3608e5cc4592b1738112653c36dac1c882c
Denis <denis18jam@hotmail.com>
2024-07-06 22:26:10 +0000

    Removed access policy

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -11,8 +11,6 @@ resource "azurerm_key_vault" "resume_key_vault" {
   sku_name = "standard"
 
   access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
       "Get",
